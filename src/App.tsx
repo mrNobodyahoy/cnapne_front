4f9950d4172f4d46a10b8e6bb1ceda88a21602e0
mrNobodyahoy <prvidal00@gmail.com>
2025-08-18 20:27:18 -0300

    criação do front-end

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold">🚀 Front inicial pronto!</h1>
+    </div>
+  )
+}
